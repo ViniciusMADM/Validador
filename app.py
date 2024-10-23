24686af3c777338c2712dfdff0e3a2ffac558ac5
ViniciusMADM <viniciusaguiarkatsunomadm@gmail.com>
2024-10-23 09:44:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,49 @@
 from flask import Flask, request, jsonify
+import logging
+import re
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
-
-def numerarCPF(cpf):
+def numerarCPF(cpf: str) -> list[int]:
     return [int(a) for a in cpf]
 
-
-def quantidade(cpf):
-    return len(cpf) == 11
-
-
-def primeiro_digito(cpf_numerado):
+def primeiro_digito(cpf_numerado: list[int]) -> bool:
     acumulador = sum(cpf_numerado[i] * (10 - i) for i in range(9))
     acumulador = (acumulador * 10) % 11
     return cpf_numerado[9] == (0 if acumulador == 10 else acumulador)
 
-
-def segundo_digito(cpf_numerado):
+def segundo_digito(cpf_numerado: list[int]) -> bool:
     acumulador = sum(cpf_numerado[i] * (11 - i) for i in range(10))
     acumulador = (acumulador * 10) % 11
     return cpf_numerado[10] == (0 if acumulador == 10 else acumulador)
 
-
-def formatar_cpf(cpf):
-    """Formata o CPF no padrão xxx.xxx.xxx-xx."""
+def formatar_cpf(cpf: str) -> str:
     return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
-
 
 @app.route('/validador_cpf', methods=['POST'])
 def validador_cpf():
     data = request.json
 
-    # Verificar se "cpf" está presente na requisição
     if 'cpf' not in data:
-        return jsonify({"valid": False, "message": "CPF inválido"}), 400
+        return jsonify({"valid": False, "message": "CPF não fornecido"}), 400
 
-    # Limpar CPF removendo pontos e traços
-    cpf = str(data.get("cpf")).replace('-', '').replace('.', '').replace('_','')
+    cpf = re.sub(r'\D', '', str(data.get("cpf")))  # Remove não dígitos
 
-    if quantidade(cpf):
+    if len(cpf) == 11:
         cpf_numerado = numerarCPF(cpf)
-
-        # Verificações de dígitos
         primeiro = primeiro_digito(cpf_numerado)
         segundo = segundo_digito(cpf_numerado)
 
-        # Log para verificar valores calculados
-        print(f"CPF Numerado: {cpf_numerado}")
-        print(f"Primeiro Dígito Calculado: {primeiro}")
-        print(f"Segundo Dígito Calculado: {segundo}")
+        logging.info(f"Validação iniciada para CPF: {cpf}")
 
         if primeiro and segundo:
-            cpf_formatado = formatar_cpf(cpf)  # Formata o CPF
+            cpf_formatado = formatar_cpf(cpf)
             return jsonify({"validação": True, "cpf": cpf_formatado}), 200
         else:
             return jsonify({"validação": False}), 200
     else:
         return jsonify({"validação": False}), 200
 
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=5000) 
+    app.run(debug=True)
